d032ecff1a66c9a9d539ff1f9ab3f5017625adfd
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-07-27 02:27:41 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,8 @@
+import {
+  AccountModel,
+  AddAccountModel
+} from '../usecases/account/add-account/db-add-account-protocols'
+
+export interface AddAccountRepository {
+  add(account: AddAccountModel): Promise<AccountModel>
+}
--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -2,14 +2,21 @@ import {
   AccountModel,
   AddAccount,
   AddAccountModel,
-  Encrypter
+  Encrypter,
+  AddAccountRepository
 } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly encrypter: Encrypter) {}
+  constructor(
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
-  async add(account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.generate(account.password)
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.generate(accountData.password)
+    await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword })
+    )
     const makeFakeAccountData = (): AccountModel => ({
       id: 'id',
       name: 'any_name',
