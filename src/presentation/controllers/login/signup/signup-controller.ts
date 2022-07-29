13a39801c47840c9e656573721de29ca83895ed8
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-07-29 16:02:37 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/login/signup/signup-controller.ts
+++ src/presentation/controllers/login/signup/signup-controller.ts
@@ -1,3 +1,4 @@
+import { AddAccount } from '../../../../domain/usecases/account/add-account'
 import { InvalidParamError, MissinParamError } from '../../../errors'
 import { badRequest, serverError } from '../../../helpers/http-helper'
 import {
@@ -8,7 +9,10 @@ import {
 } from '../../../protocols/'
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -23,7 +27,7 @@ export class SignUpController implements Controller {
           return badRequest(new MissinParamError(field))
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
@@ -31,6 +35,11 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      await this.addAccount.add({
+        name,
+        email,
+        password
+      })
       return {
         statusCode: 200,
         body: 'Cool body'
