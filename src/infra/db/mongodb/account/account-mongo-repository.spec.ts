fac3b252e0c64f69e49652e980ed764dbdcab4c1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-07-29 11:55:56 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -6,6 +6,11 @@ describe('AccountMongoRepository', () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
