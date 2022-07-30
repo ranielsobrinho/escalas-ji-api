2a215e5bd51a6ee8639ceb2cdadfdf85b4abcc54
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-07-30 12:47:25 -0300

    refactor: move required field validation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,14 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter/bcrypt-ad
 import { AccountMongoRepository } from '../../infra/db/mongodb/account/account-mongo-repository'
 import { SignUpController } from '../../presentation/controllers/login/signup/signup-controller'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const emailValidator = new EmailValidatorAdapter()
   const encrypter = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
+  const validationComposite = makeSignUpValidation()
   const addAccount = new DbAddAccount(encrypter, addAccountRepository)
-  return new SignUpController(emailValidator, addAccount)
+  return new SignUpController(emailValidator, addAccount, validationComposite)
 }
