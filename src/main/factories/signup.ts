e492b4c7e8f28e0e389482a87682ff57fa4ac40c
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-07-30 11:13:37 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/usecases/account/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account/account-mongo-repository'
+import { SignUpController } from '../../presentation/controllers/login/signup/signup-controller'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidator = new EmailValidatorAdapter()
+  const encrypter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, addAccountRepository)
+  return new SignUpController(emailValidator, addAccount)
+}
