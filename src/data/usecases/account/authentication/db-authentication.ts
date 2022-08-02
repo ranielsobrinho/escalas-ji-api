1311364cf9a0e16cf63c70222547ef0e5cf710ea
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-02 11:22:41 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct value

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,11 +1,13 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
 import { HashComparer } from '../../../protocols/hash-comparer'
 import { LoadAccountByEmailRepository } from '../../../protocols/load-account-by-email-repository'
+import { TokenGenerator } from '../../../protocols/token-generator'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth({
@@ -14,7 +16,13 @@ export class DbAuthentication implements Authentication {
   }: Authentication.Params): Promise<Authentication.Result> {
     const acccount = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (acccount) {
-      await this.hashComparer.compare(password, acccount.password)
+      const isValid = await this.hashComparer.compare(
+        password,
+        acccount.password
+      )
+      if (isValid) {
+        await this.tokenGenerator.generate(acccount.id)
+      }
     }
     return Promise.resolve(null)
   }
