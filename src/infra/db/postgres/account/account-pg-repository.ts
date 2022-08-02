787513c8c30a2157e3919ece3b2f0023e08f00bf
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-02 16:22:27 -0300

    feat: ensure AccountPgRepository returns an account if loadByEmail succeeds

--- src/infra/db/postgres/account/account-pg-repository.ts
+++ src/infra/db/postgres/account/account-pg-repository.ts
@@ -4,8 +4,11 @@ import { AddAccountModel } from '../../../../domain/usecases/account/add-account
 import { map } from './account-mapper'
 
 import { PrismaClient } from '@prisma/client'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 
-export class AccountPgRepository implements AddAccountRepository {
+export class AccountPgRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   constructor(private readonly prisma: PrismaClient) {}
 
   async add(account: AddAccountModel): Promise<AccountModel> {
@@ -17,5 +20,14 @@ export class AccountPgRepository implements AddAccountRepository {
       }
     })
     return map(create)
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const account = await this.prisma.users.findFirst({
+      where: {
+        email
+      }
+    })
+    return map(account)
   }
 }
