6f9b49dac6a1f28ab81087b6c56c84bf61b1deac
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-02 16:23:04 -0300

    test: ensure AccountPgRepository returns an account if loadByEmail succeeds

--- src/infra/db/postgres/account/account-pg-repository.spec.ts
+++ src/infra/db/postgres/account/account-pg-repository.spec.ts
@@ -24,13 +24,34 @@ describe('AccountPgRepository', () => {
     return new AccountPgRepository(prisma)
   }
 
-  test('Should return an account on success', async () => {
-    const sut = makeSut()
-    const account = await sut.add(makeFakeAddAccountModel())
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+  describe('add()', () => {
+    test('Should return an account on success', async () => {
+      const sut = makeSut()
+      const account = await sut.add(makeFakeAddAccountModel())
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
+
+  describe('loadByEmail()', () => {
+    test('Should return an account if loadByEmail succeeds', async () => {
+      const sut = makeSut()
+      await prisma.users.create({
+        data: {
+          name: 'valid_name',
+          email: 'valid_email@mail.com',
+          password: 'hashed_password'
+        }
+      })
+      const account = await sut.loadByEmail('valid_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('valid_name')
+      expect(account.email).toBe('valid_email@mail.com')
+      expect(account.password).toBe('hashed_password')
+    })
   })
 })
