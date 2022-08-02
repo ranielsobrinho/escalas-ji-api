f6e71d662f5c9483af2790edc67b68c81610fc75
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-02 16:08:52 -0300

    test: ensure AccountMongoRepository returns an account if loadByEmail succeeds

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -1,13 +1,15 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-mongo-repository'
 
+let accountCollection: Collection
 describe('AccountMongoRepository', () => {
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL as string)
+    await MongoHelper.connect(process.env.MONGO_URL)
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -19,17 +21,36 @@ describe('AccountMongoRepository', () => {
     return new AccountMongoRepository()
   }
 
-  test('Should return an account on success', async () => {
-    const sut = makeSut()
-    const account = await sut.add({
-      name: 'valid_name',
-      email: 'valid_email@mail.com',
-      password: 'valid_password'
+  describe('add()', () => {
+    test('Should return an account on success', async () => {
+      const sut = makeSut()
+      const account = await sut.add({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password'
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toEqual('valid_name')
+      expect(account.email).toEqual('valid_email@mail.com')
+      expect(account.password).toEqual('valid_password')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toEqual('valid_name')
-    expect(account.email).toEqual('valid_email@mail.com')
-    expect(account.password).toEqual('valid_password')
+  })
+
+  describe('loadByEmail()', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password'
+      })
+      const account = await sut.loadByEmail('valid_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toEqual('valid_name')
+      expect(account.email).toEqual('valid_email@mail.com')
+      expect(account.password).toEqual('valid_password')
+    })
   })
 })
