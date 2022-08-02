bc9a9f15024150c4bb5ddb1b7c1a235f0f4a0ce1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-02 02:51:20 -0300

    refactor: remove unused code

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -13,14 +13,5 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name)
-  },
-
-  map: (collection: any): any => {
-    const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, ...collectionWithoutId, { id: _id })
-  },
-
-  mapCollection: (collection: any[]): any[] => {
-    return collection.map((c) => MongoHelper.map(c))
   }
 }
