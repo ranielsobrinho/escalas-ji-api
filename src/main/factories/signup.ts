5e9a89babb91cc67082b234524b8843c48a721a2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-03 01:27:21 -0300

    refactor: using postgres instead of mongoDb

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,13 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import { DbAddAccount } from '../../data/usecases/account/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account/account-mongo-repository'
+import { AccountPgRepository } from '../../infra/db/postgres/account/account-pg-repository'
 import { SignUpController } from '../../presentation/controllers/login/signup/signup-controller'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
-  const addAccountRepository = new AccountMongoRepository()
+  const prismaClient = new PrismaClient()
+  const addAccountRepository = new AccountPgRepository(prismaClient)
   const validationComposite = makeSignUpValidation()
   const addAccount = new DbAddAccount(encrypter, addAccountRepository)
   return new SignUpController(addAccount, validationComposite)
