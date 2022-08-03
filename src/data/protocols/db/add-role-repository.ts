eca4621aee30578961109fa3087f148ed6c704b4
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-03 18:10:48 -0300

    feat: ensure DbAddRole calls AddRoleRepository with correct values

---
+++ src/data/protocols/db/add-role-repository.ts
@@ -0,0 +1,5 @@
+import { RoleModel } from '../../../domain/models/role'
+
+export interface AddRoleRepository {
+  add(name: string, userId: string): Promise<RoleModel>
+}
--- src/data/usecases/role/add-role/db-add-role.ts
+++ src/data/usecases/role/add-role/db-add-role.ts
@@ -1,13 +1,18 @@
 import { AddRole } from '../../../../domain/usecases/roles/add-roles'
+import { AddRoleRepository } from '../../../protocols/db/add-role-repository'
 import { LoadAccountByIdRepository } from '../../../protocols/db/load-account-by-id-repository'
 
 export class DbAddRole implements AddRole {
   constructor(
-    private readonly loadAccountByIdRepository: LoadAccountByIdRepository
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+    private readonly addRoleRepository: AddRoleRepository
   ) {}
 
   async add({ name, userId }: AddRole.Params): Promise<AddRole.Result> {
-    await this.loadAccountByIdRepository.loadById(userId)
+    const isValid = await this.loadAccountByIdRepository.loadById(userId)
+    if (isValid) {
+      await this.addRoleRepository.add(name, userId)
+    }
     return null
   }
 }
