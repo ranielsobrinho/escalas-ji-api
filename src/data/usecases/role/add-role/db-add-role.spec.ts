196d8926bb0ddfc29ed9f40c664f09f74f7fe478
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-03 17:49:09 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/role/add-role/db-add-role.spec.ts
+++ src/data/usecases/role/add-role/db-add-role.spec.ts
@@ -1,15 +1,32 @@
 import { LoadAccountByIdRepository } from '../../../protocols/db/load-account-by-id-repository'
 import { DbAddRole } from './db-add-role'
 
+const makeFakeLoadById = (): LoadAccountByIdRepository => {
+  class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
+    async loadById(id: string): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+  }
+  return new LoadAccountByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbAddRole
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByIdRepositoryStub = makeFakeLoadById()
+  const sut = new DbAddRole(loadAccountByIdRepositoryStub)
+  return {
+    sut,
+    loadAccountByIdRepositoryStub
+  }
+}
+
 describe('DbAddRole', () => {
   test('Should call LoadAccountByIdRepository with correct values', async () => {
-    class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
-      async loadById(id: string): Promise<boolean> {
-        return Promise.resolve(true)
-      }
-    }
-    const loadAccountByIdRepositoryStub = new LoadAccountByIdRepositoryStub()
-    const sut = new DbAddRole(loadAccountByIdRepositoryStub)
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadAccountByIdRepositoryStub, 'loadById')
     await sut.add({
       name: 'any_name',
