110693ba0510d584baea7ef73152966fc00447e4
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-03 02:12:24 -0300

    feat: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,5 +1,5 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
-import { unauthorized } from '../../../helpers/http-helper'
+import { serverError, unauthorized } from '../../../helpers/http-helper'
 import {
   Controller,
   HttpRequest,
@@ -10,11 +10,15 @@ export class LoginController implements Controller {
   constructor(private readonly authentication: Authentication) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    const accessToken = await this.authentication.auth({ email, password })
-    if (!accessToken) {
-      return unauthorized()
+    try {
+      const { email, password } = httpRequest.body
+      const accessToken = await this.authentication.auth({ email, password })
+      if (!accessToken) {
+        return unauthorized()
+      }
+      return null
+    } catch (error) {
+      return serverError()
     }
-    return null
   }
 }
