a43689c6f57aca99950ba9b2b52867766dc72e3d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-03 02:28:58 -0300

    feat: ensure LoginController returns 400 if Validation returns a error

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,16 +1,29 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
-import { ok, serverError, unauthorized } from '../../../helpers/http-helper'
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized
+} from '../../../helpers/http-helper'
 import {
   Controller,
   HttpRequest,
-  HttpResponse
+  HttpResponse,
+  Validation
 } from '../signup/signup-controller-protocols'
 
 export class LoginController implements Controller {
-  constructor(private readonly authentication: Authentication) {}
+  constructor(
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { email, password } = httpRequest.body
       const accessToken = await this.authentication.auth({ email, password })
       if (!accessToken) {
