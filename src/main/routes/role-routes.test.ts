d657b939b30edc36ff36d367e6c41af5a7927c60
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 03:35:42 -0300

    test: ensure add role return a role on success

---
+++ src/main/routes/role-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import app from '../config/app'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await prisma.$connect()
+  })
+
+  beforeEach(async () => {
+    await prisma.users.deleteMany({ where: { email: 'valid_email@mail.com' } })
+  })
+
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  describe('POST /add-role', () => {
+    test('Should return a role on success', async () => {
+      const account = await prisma.users.create({
+        data: {
+          name: 'valid_name',
+          email: 'valid_email@mail.com',
+          password: 'hashed_password',
+          isadmin: false
+        }
+      })
+      await request(app)
+        .post('/api/add-role')
+        .send({
+          name: 'any_name',
+          userId: account.id.toString()
+        })
+        .expect(200)
+    })
+  })
+})
