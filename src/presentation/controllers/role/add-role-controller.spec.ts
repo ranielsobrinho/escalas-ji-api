93cd1ce407774a7b235b1591eb2c207c73295982
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 02:58:57 -0300

    test: ensure AddRoleController returns 400 if Validation returns a error

--- src/presentation/controllers/role/add-role-controller.spec.ts
+++ src/presentation/controllers/role/add-role-controller.spec.ts
@@ -1,7 +1,7 @@
 import { RoleModel } from '../../../domain/models/role'
 import { AddRole } from '../../../domain/usecases/roles/add-roles'
-import { ServerError } from '../../errors'
-import { ok } from '../../helpers/http-helper'
+import { MissingParamError, ServerError } from '../../errors'
+import { badRequest, ok } from '../../helpers/http-helper'
 import { HttpRequest } from '../../protocols'
 import { Validation } from '../login/login/login-controller-protocols'
 import { AddRoleController } from './add-role-controller'
@@ -95,5 +95,14 @@ describe('AddRoleController', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns a error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
