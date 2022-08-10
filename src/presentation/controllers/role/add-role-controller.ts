8e848beeada5dbe30f6f82425cec138cd38d411e
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 02:31:55 -0300

    feat: ensure AddRoleController returns 500 if AddRole throws

--- src/presentation/controllers/role/add-role-controller.ts
+++ src/presentation/controllers/role/add-role-controller.ts
@@ -1,12 +1,17 @@
 import { AddRole } from '../../../domain/usecases/roles/add-roles'
+import { serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class AddRoleController implements Controller {
   constructor(private readonly addRole: AddRole) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, userId } = httpRequest.body
-    await this.addRole.add({ name, userId })
-    return Promise.resolve(null)
+    try {
+      const { name, userId } = httpRequest.body
+      await this.addRole.add({ name, userId })
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
