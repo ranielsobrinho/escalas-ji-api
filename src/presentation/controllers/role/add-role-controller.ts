3a9a1d14de975c1b4443bd02f3a4c7bea861b1fa
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 02:55:58 -0300

    feat: ensure AddRoleController calls Validation with correct values

--- src/presentation/controllers/role/add-role-controller.ts
+++ src/presentation/controllers/role/add-role-controller.ts
@@ -1,12 +1,17 @@
 import { AddRole } from '../../../domain/usecases/roles/add-roles'
 import { serverError, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Validation } from '../login/login/login-controller-protocols'
 
 export class AddRoleController implements Controller {
-  constructor(private readonly addRole: AddRole) {}
+  constructor(
+    private readonly addRole: AddRole,
+    private readonly validation: Validation
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { name, userId } = httpRequest.body
       const role = await this.addRole.add({ name, userId })
       return ok(role)
