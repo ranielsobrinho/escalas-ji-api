a5c349537cc121c0d7fa539fd328608e086ba10f
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 11:11:56 -0300

    test: ensure JwtAdpater throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -53,5 +53,14 @@ describe('JWTAdapter', () => {
       const token = await sut.decrypt('any_token')
       expect(token).toBe('any_value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
