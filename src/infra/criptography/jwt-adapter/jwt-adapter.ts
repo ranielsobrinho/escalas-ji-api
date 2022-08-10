c957dacc52273f3aa3dcedef38728c53f130f0e2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 11:29:03 -0300

    refactor: ensure JwtAdapter implements TokenGenerator interface

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,12 +1,12 @@
-import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 import { Decrypter } from '../../../data/protocols/criptography/decrypter'
 import jwt from 'jsonwebtoken'
+import { TokenGenerator } from '../../../data/protocols/criptography/token-generator'
 
-export class JWTAdapter implements Encrypter, Decrypter {
+export class JWTAdapter implements TokenGenerator, Decrypter {
   constructor(private readonly salt: string) {}
 
-  async generate(value: string): Promise<string> {
-    const accessToken = jwt.sign({ id: value }, this.salt)
+  async generate(id: string): Promise<string> {
+    const accessToken = jwt.sign({ id: id }, this.salt)
     return accessToken
   }
 
