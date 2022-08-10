8f9703babdea5111e5cb47b34543b7af155f4762
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-10 15:45:16 -0300

    temp: remove auth middleware

--- src/main/routes/role-routes.ts
+++ src/main/routes/role-routes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddRoleController } from '../factories/controllers/role/add-role/add-role'
-import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  router.post('/add-role', auth, adaptRoute(makeAddRoleController()))
+  router.post('/add-role', adaptRoute(makeAddRoleController()))
 }
