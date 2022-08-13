3a51dc9522ef8e3cd9f4d420fdafd17cfb2ba759
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-13 11:25:29 -0300

    refactor: ensure add-role route returns 403 if no accessToken is provided

--- src/main/routes/role-routes.test.ts
+++ src/main/routes/role-routes.test.ts
@@ -17,7 +17,7 @@ describe('Login Routes', () => {
   })
 
   describe('POST /add-role', () => {
-    test('Should return 200 add role', async () => {
+    test('Should return 403 on add role without accessToken', async () => {
       const account = await prisma.users.create({
         data: {
           name: 'valid_name',
@@ -32,7 +32,7 @@ describe('Login Routes', () => {
           name: 'any_name',
           userId: account.id.toString()
         })
-        .expect(200)
+        .expect(403)
     })
   })
 })
