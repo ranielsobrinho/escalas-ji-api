9404ef8edb6abcad4862920a8518c61855fe7ae1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-13 11:24:56 -0300

    refactor: ensure makeDbLoadAccountByToken implements the right usecase

--- src/main/factories/usecases/account/load-by-token/db-load-by-token-factory.ts
+++ src/main/factories/usecases/account/load-by-token/db-load-by-token-factory.ts
@@ -1,9 +1,9 @@
-import { DbLoadAccountById } from '../../../../../data/usecases/account/load-account-by-id/db-load-account-by-id'
-import { LoadAccountById } from '../../../../../domain/usecases/account/load-account-by-id'
+import { LoadAccountByToken } from '../../../../../domain/usecases/account/load-account-by-token'
+import { DbLoadAccountByToken } from '../../../../../data/usecases/account/load-account-by-token/db-load-account-by-token'
 import { JWTAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
 import env from '../../../../config/env'
 
-export const makeDbLoadAccountByToken = (): LoadAccountById => {
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
   const jwtAdapter = new JWTAdapter(env.jwtSecret)
-  return new DbLoadAccountById(jwtAdapter)
+  return new DbLoadAccountByToken(jwtAdapter)
 }
