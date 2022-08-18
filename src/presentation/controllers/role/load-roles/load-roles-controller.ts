7bc03c3fefc9e06823beeae3139163405e741a98
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-18 18:52:30 -0300

    feat: ensure LoadRoleController returns 200 on success

--- src/presentation/controllers/role/load-roles/load-roles-controller.ts
+++ src/presentation/controllers/role/load-roles/load-roles-controller.ts
@@ -1,13 +1,13 @@
 import { LoadRoles } from '../../../../domain/usecases/roles/load-roles'
-import { serverError } from '../../../helpers/http-helper'
+import { serverError, ok } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadRolesController implements Controller {
   constructor(private readonly loadRoles: LoadRoles) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadRoles.load()
-      return Promise.resolve(null)
+      const roles = await this.loadRoles.load()
+      return ok(roles)
     } catch (error) {
       return serverError(error)
     }
