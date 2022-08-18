960df847dbe27aed8a4e61b66d624b0ae01daa1d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-18 18:39:30 -0300

    feat: ensure LoadRolesController returns 500 if LoadRoles throws

--- src/presentation/controllers/role/load-roles/load-roles-controller.ts
+++ src/presentation/controllers/role/load-roles/load-roles-controller.ts
@@ -1,10 +1,15 @@
 import { LoadRoles } from '../../../../domain/usecases/roles/load-roles'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadRolesController implements Controller {
   constructor(private readonly loadRoles: LoadRoles) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadRoles.load()
-    return Promise.resolve(null)
+    try {
+      await this.loadRoles.load()
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
