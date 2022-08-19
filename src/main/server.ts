0e4126ddaaed49172d249ad01ef457dff0df7de2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-19 01:47:56 -0300

    refactor: remove mongohelper connect

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,6 @@
 import env from './config/env'
-import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import app from './config/app'
 
-MongoHelper.connect(env.mongoUrl)
-  .then(async () => {
-    const app = (await import('./config/app')).default
-    app.listen(env.port, () =>
-      console.log(`Server running on http://localhost:${env.port}`)
-    )
-  })
-  .catch(console.error)
+app.listen(env.port, () =>
+  console.log(`Server running on http://localhost:${env.port}`)
+)
