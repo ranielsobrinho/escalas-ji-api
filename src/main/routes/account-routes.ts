248c51b17b9553970488dbe40cd0795d22c9c162
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-20 16:36:08 -0300

    feat: should return 403 if no accessToken is provided

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeLoadAccountsController } from '../factories/controllers/accounts/load-accounts/load-accounts'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  router.get('/load-accounts', adaptRoute(makeLoadAccountsController()))
+  router.get('/load-accounts', auth, adaptRoute(makeLoadAccountsController()))
 }
