31882af8542944d39792e74c1af2dfab21669fe8
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-20 16:03:33 -0300

    feat: ensure LoadAccountsController returns 500 if LoadAccounts throws

--- src/presentation/controllers/accounts/load-accounts/load-accounts-controller.ts
+++ src/presentation/controllers/accounts/load-accounts/load-accounts-controller.ts
@@ -1,10 +1,15 @@
 import { LoadAccounts } from '../../../../domain/usecases/account/load-accounts'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadAccountsController implements Controller {
   constructor(private readonly loadAccounts: LoadAccounts) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadAccounts.load()
-    return Promise.resolve(null)
+    try {
+      await this.loadAccounts.load()
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
