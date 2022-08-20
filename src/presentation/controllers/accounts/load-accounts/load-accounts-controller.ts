c97d0ba596c35b75bd950650c517b4888b1213c7
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-20 16:14:58 -0300

    feat: ensure LoadAccountsController returns 200 on success

--- src/presentation/controllers/accounts/load-accounts/load-accounts-controller.ts
+++ src/presentation/controllers/accounts/load-accounts/load-accounts-controller.ts
@@ -1,13 +1,13 @@
 import { LoadAccounts } from '../../../../domain/usecases/account/load-accounts'
-import { serverError } from '../../../helpers/http-helper'
+import { serverError, ok } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadAccountsController implements Controller {
   constructor(private readonly loadAccounts: LoadAccounts) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadAccounts.load()
-      return null
+      const accounts = await this.loadAccounts.load()
+      return ok(accounts)
     } catch (error) {
       return serverError(error)
     }
