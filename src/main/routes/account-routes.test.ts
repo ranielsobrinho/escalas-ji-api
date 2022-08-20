732032aa994dc9bcc443bb0f33554ad2a9fd4aa7
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-20 16:36:23 -0300

    test: should return 403 if no acessToken is provided

--- src/main/routes/account-routes.test.ts
+++ src/main/routes/account-routes.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest'
 import app from '../config/app'
 import { PrismaClient } from '@prisma/client'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 const prisma = new PrismaClient()
 describe('Accounts Routes', () => {
@@ -9,7 +11,7 @@ describe('Accounts Routes', () => {
   })
 
   beforeEach(async () => {
-    await prisma.users.deleteMany({ where: { email: 'any_email@mail.com' } })
+    await prisma.users.deleteMany({ where: { email: 'valid_email@mail.com' } })
   })
 
   afterAll(async () => {
@@ -17,8 +19,24 @@ describe('Accounts Routes', () => {
   })
 
   describe('GET /load-accounts', () => {
-    test('Should return accounts on success', async () => {
-      await request(app).get('/api/load-accounts').expect(200)
+    test('Should return 403 if no accessToken is provided', async () => {
+      await request(app).get('/api/load-accounts').expect(403)
+    })
+
+    test('Should return 200 if accessToken is provided', async () => {
+      const account = await prisma.users.create({
+        data: {
+          name: 'valid_name',
+          email: 'valid_email@mail.com',
+          password: 'hashed_password',
+          isadmin: false
+        }
+      })
+      const accessToken = sign(account.id.toString(), env.jwtSecret)
+      await request(app)
+        .get('/api/load-accounts')
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
