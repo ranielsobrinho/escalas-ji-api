9a24143855f26427560cdebefb488a684446ba51
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-24 12:15:51 -0300

    feat: ensure AddScaleMusicController calls Validation with correct values

--- src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.ts
+++ src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.ts
@@ -1,11 +1,16 @@
 import { AddScaleMusic } from '../../../../domain/usecases/scale-music/add-scale-music'
 import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import { Validation } from '../../../helpers/validators/validation'
 
 export class AddScaleMusicController implements Controller {
-  constructor(private readonly addScaleMusic: AddScaleMusic) {}
+  constructor(
+    private readonly addScaleMusic: AddScaleMusic,
+    private readonly validation: Validation
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { music_link } = httpRequest.body
       await this.addScaleMusic.add(music_link)
       return noContent()
