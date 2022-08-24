924841b2b04cf3f64f75d06e36b2722cbcc07af2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-24 11:51:26 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.spec.ts
+++ src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.spec.ts
@@ -2,15 +2,32 @@ import { AddScaleMusic } from '../../../../domain/usecases/scale-music/add-scale
 import { HttpRequest } from '../../../protocols'
 import { AddScaleMusicController } from './add-scale-music-controller'
 
+const makeFakeAddScale = (): AddScaleMusic => {
+  class AddScaleMusicStub implements AddScaleMusic {
+    async add(params: AddScaleMusic.Params): Promise<void> {
+      return Promise.resolve(null)
+    }
+  }
+  return new AddScaleMusicStub()
+}
+
+type SutTypes = {
+  sut: AddScaleMusicController
+  addScaleMusicStub: AddScaleMusic
+}
+
+const makeSut = (): SutTypes => {
+  const addScaleMusicStub = makeFakeAddScale()
+  const sut = new AddScaleMusicController(addScaleMusicStub)
+  return {
+    sut,
+    addScaleMusicStub
+  }
+}
+
 describe('AddScaleMusicController', () => {
   test('Should call AddScaleMusic with correct values', async () => {
-    class AddScaleMusicStub implements AddScaleMusic {
-      async add(params: AddScaleMusic.Params): Promise<void> {
-        return Promise.resolve(null)
-      }
-    }
-    const addScaleMusicStub = new AddScaleMusicStub()
-    const sut = new AddScaleMusicController(addScaleMusicStub)
+    const { sut, addScaleMusicStub } = makeSut()
     const addScaleSpy = jest.spyOn(addScaleMusicStub, 'add')
     const httpRequest: HttpRequest = {
       body: {
