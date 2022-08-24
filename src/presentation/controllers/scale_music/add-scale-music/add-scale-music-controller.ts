06abcb01a73d7d3307ec438217bd6a701e06ef09
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-24 12:05:52 -0300

    feat: ensure AddScaleMusicController returns 500 if AddScaleMusic throws

--- src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.ts
+++ src/presentation/controllers/scale_music/add-scale-music/add-scale-music-controller.ts
@@ -1,11 +1,16 @@
 import { AddScaleMusic } from '../../../../domain/usecases/scale-music/add-scale-music'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class AddScaleMusicController implements Controller {
   constructor(private readonly addScaleMusic: AddScaleMusic) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { music_link } = httpRequest.body
-    await this.addScaleMusic.add(music_link)
-    return Promise.resolve(null)
+    try {
+      const { music_link } = httpRequest.body
+      await this.addScaleMusic.add(music_link)
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
