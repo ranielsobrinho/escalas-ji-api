ec27fb799aa2f958e380b2620dfd2d97b5ec51e1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-25 14:31:06 -0300

    feat: ensure LoadScaleMusicController returns 500 if LoadScaleMusic throws

--- src/presentation/controllers/scale_music/load-scale-music/load-scale-music-controller.ts
+++ src/presentation/controllers/scale_music/load-scale-music/load-scale-music-controller.ts
@@ -2,14 +2,19 @@ import { LoadScaleMusics } from '../../../../domain/usecases/scale-music/load-sc
 import {
   Controller,
   HttpRequest,
-  HttpResponse
+  HttpResponse,
+  serverError
 } from '../add-scale-music/add-scale-music-controller-protocols'
 
 export class LoadScaleMusicController implements Controller {
   constructor(private readonly loadScaleMusic: LoadScaleMusics) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadScaleMusic.load()
-    return Promise.resolve(null)
+    try {
+      await this.loadScaleMusic.load()
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
