8b197ef2074019c406a867235fe2c21904cd9ca5
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-08-25 11:36:01 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/scale-music/load-scale-music/db-load-scale-music.spec.ts
+++ src/data/usecases/scale-music/load-scale-music/db-load-scale-music.spec.ts
@@ -15,16 +15,33 @@ const makeScaleMusic = (): ScaleMusic[] => {
   ]
 }
 
+const makeLoadScaleMusicRepository = (): LoadScaleMusicRepository => {
+  class LoadScaleMusicRepositoryStub implements LoadScaleMusicRepository {
+    async load(): Promise<ScaleMusic[]> {
+      return Promise.resolve(makeScaleMusic())
+    }
+  }
+  return new LoadScaleMusicRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadScaleMusic
+  loadScaleMusicRepositoryStub: LoadScaleMusicRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadScaleMusicRepositoryStub = makeLoadScaleMusicRepository()
+  const sut = new DbLoadScaleMusic(loadScaleMusicRepositoryStub)
+  return {
+    sut,
+    loadScaleMusicRepositoryStub
+  }
+}
+
 describe('DbLoadScaleMusic', () => {
   test('Should call LoadScaleMusicRepository once', async () => {
-    class LoadScaleMusicRepositoryStub implements LoadScaleMusicRepository {
-      async load(): Promise<ScaleMusic[]> {
-        return Promise.resolve(makeScaleMusic())
-      }
-    }
-    const loadScaleMusicStub = new LoadScaleMusicRepositoryStub()
-    const sut = new DbLoadScaleMusic(loadScaleMusicStub)
-    const loadSpy = jest.spyOn(loadScaleMusicStub, 'load')
+    const { sut, loadScaleMusicRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadScaleMusicRepositoryStub, 'load')
     await sut.load()
     expect(loadSpy).toHaveBeenCalled()
     expect(loadSpy).toHaveBeenCalledTimes(1)
