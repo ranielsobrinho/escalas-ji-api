5d89cc8272caa747a806c097c461af2ecd92cbdd
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-03 01:30:34 -0300

    feat: ensure AddScaleController calls Validation with correct values

--- src/presentation/controllers/scale/add-scale/add-scale-controller.ts
+++ src/presentation/controllers/scale/add-scale/add-scale-controller.ts
@@ -1,12 +1,17 @@
 import { AddScale } from '../../../../domain/usecases/scale/add-scale'
 import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import { Validation } from '../../login/login/login-controller-protocols'
 
 export class AddScaleController implements Controller {
-  constructor(private readonly addScale: AddScale) {}
+  constructor(
+    private readonly addScale: AddScale,
+    private readonly validation: Validation
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const {
         singers,
         bass,
