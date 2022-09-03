975eec7d031d44ebc814d14e28ce4590e9f34984
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-03 01:20:54 -0300

    feat: ensure AddScaleController returns 500 if AddScale throws

--- src/presentation/controllers/scale/add-scale/add-scale-controller.ts
+++ src/presentation/controllers/scale/add-scale/add-scale-controller.ts
@@ -1,30 +1,35 @@
 import { AddScale } from '../../../../domain/usecases/scale/add-scale'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class AddScaleController implements Controller {
   constructor(private readonly addScale: AddScale) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const {
-      singers,
-      bass,
-      guitar,
-      acousticGuitar,
-      keyboard,
-      drum,
-      musics,
-      date
-    } = httpRequest.body
-    await this.addScale.add({
-      singers,
-      bass,
-      guitar,
-      acousticGuitar,
-      keyboard,
-      drum,
-      musics,
-      date
-    })
-    return Promise.resolve(null)
+    try {
+      const {
+        singers,
+        bass,
+        guitar,
+        acousticGuitar,
+        keyboard,
+        drum,
+        musics,
+        date
+      } = httpRequest.body
+      await this.addScale.add({
+        singers,
+        bass,
+        guitar,
+        acousticGuitar,
+        keyboard,
+        drum,
+        musics,
+        date
+      })
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
