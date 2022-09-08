efe764768a30dcfa4b6fa1cd1fa0f10130d35577
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-08 18:15:53 -0300

    feat: add morgan to log requests

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import morgan from 'morgan'
+
+morgan.token('body', (req: express.Request) => {
+  const isNotGet = req.method !== 'GET'
+  const isNotAudio = !req.url.includes('audios')
+  if (isNotGet && isNotAudio) {
+    return JSON.stringify(req.body)
+  }
+  return 'body-empty'
+})
 
 const app = express()
+app.use(morgan(':date[iso] :method :url :status :body - :total-time ms'))
 setupMiddlewares(app)
 setupRoutes(app)
 
